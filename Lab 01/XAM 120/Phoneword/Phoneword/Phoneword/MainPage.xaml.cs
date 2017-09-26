8773256762ab22b6601555b894a065ae0c2724d1
umino <umino.sharp@gmail.com>
2017-09-26 20:13:45 +0700

    Lab 06: Xamarin Form (Introduce Projects)

---
+++ Lab 01/XAM 120/Phoneword/Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+	public partial class MainPage : ContentPage
+	{
+        private string translatedNumber;
+        public MainPage()
+        {
+            InitializeComponent();
+            translateButton.Clicked += OnTranslate;
+            callButton.Clicked += OnCall;
+        }
+
+        private async void OnCall(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Dial a Number", "Would you to call " + translatedNumber + " ?", "Yes", "No"))
+            {
+                var dialer = DependencyService.Get<IDialer>();
+                if (dialer != null)
+                {
+                    await dialer.DialAsync(translatedNumber);
+                }
+            }
+
+
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call" + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+
+    }
+}
